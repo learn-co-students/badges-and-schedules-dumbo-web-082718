71eb45d0d65767407c5e0af93b0267d1a57b3625
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 09:21:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,9 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.collect do |name|
-    # badge_maker(name)
-    puts "Hello, my name is #{name}"
-  end
-  return names 
-
+def batch_badge_creator(names)
+  names.collect {|name| badge_maker(name)}
 end
 
 def assign_rooms(attendees)
